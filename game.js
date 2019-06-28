94e9b17da58fe565d279bc23f82f4b68ac6c5509
Sheheryar Parvaz <skipper308@hotmail.ca>
2019-06-27 22:32:38 -0400

    Initial implementation

--- game.js
+++ game.js
@@ -2,7 +2,7 @@
 
 const PIECES = {
   I: { clr: '#c1f2f9', blk: [[-1,+0], [+0,+0], [+1,+0], [+2,+0]] },
-  O: { clr: '#e4fbb3', blk: [[+0,+0], [-1,+0], [-1,+1], [+0,+1]] },
+  O: { clr: '#e4fbb3', blk: [[+0,+0], [+1,+0], [+1,+1], [+0,+1]] },
   T: { clr: '#ebbab9', blk: [[+0,+0], [-1,+0], [+0,+1], [+1,+0]] },
   J: { clr: '#75b9be', blk: [[-1,+1], [-1,+0], [+0,+0], [+1,+0]] },
   L: { clr: '#ffc09f', blk: [[-1,+0], [+0,+0], [+1,+0], [+1,+1]] },
@@ -10,43 +10,159 @@ const PIECES = {
   Z: { clr: '#e34a6f', blk: [[-1,+1], [+0,+1], [+0,+0], [+1,+0]] },
 };
 
+const PIECE_NAMES = Object.keys(PIECES);
+
+function offset_tbl(piece_name, state) {
+  switch (piece_name) {
+    case 'T':
+    case 'J':
+    case 'L':
+    case 'S':
+    case 'Z':
+      return [
+        [[+0,+0], [+0,+0], [+0,+0], [+0,+0], [+0,+0]],
+        [[+0,+0], [+1,+0], [+1,-1], [+0,+2], [+1,+2]],
+        [[+0,+0], [+0,+0], [+0,+0], [+0,+0], [+0,+0]],
+        [[+0,+0], [-1,+0], [-1,-1], [+0,+2], [-1,+2]],
+      ][state];
+      break;
+    case 'I':
+      return [
+        [[+0,+0], [-1,+0], [+2,+0], [-1,+0], [+2,+0]],
+        [[-1,+0], [+0,+0], [+0,+0], [+0,+1], [+0,-2]],
+        [[-1,+1], [+1,+1], [-2,+1], [+1,+0], [-2,+0]],
+        [[+0,+1], [+0,+1], [+0,+1], [+0,-1], [+0,+2]],
+      ][state];
+      break;
+    case 'O':
+      return [
+        [[+0,+0]],
+        [[+0,-1]],
+        [[-1,-1]],
+        [[-1,+0]],
+      ][state];
+      break;
+  }
+}
+
+function get_offsets(piece_name, prev_st, next_st) {
+  let r = []; // return val
+  const ps = offset_tbl(piece_name, prev_st);
+  const ns = offset_tbl(piece_name, next_st);
+
+  for (let i = 0; i < ns.length; ++i) {
+    let [px, py] = ps[i];
+    let [nx, ny] = ns[i];
+
+    r[i] = [px - nx, py - ny];
+  }
+
+  return r;
+}
+
 class Field {
   constructor(id, [width, height], bg_clr) {
     this.cnv = document.getElementById(id);
     this.ctx = this.cnv.getContext('2d', { alpha: false });
 
-    let field = new Array(width).fill(false)
-      .map(() => new Uint8Array(height).fill(false));
+    this.field = new Array(width).fill(0)
+      .map(() => new Uint8Array(height).fill(0));
 
     Object.defineProperties(this, {
       width:     { writable: false, value: width },
       height:    { writable: false, value: height },
       bg_clr:    { writable: false, value: bg_clr },
       blk_width: { writable: false, value: this.cnv.width/width },
-      field:     { writable: false, value: field },
     });
   }
 
   clear() {
-    this.field.map((x) => x.fill(false));
+    this.field = this.field.map((x) => x.fill(0));
 
     this.ctx.fillStyle = this.bg_clr;
     this.ctx.fillRect(0, 0, this.cnv.width, this.cnv.height);
   }
 
-  _write_blk([x, y], val, clr) {
-    this.field[x][y] = val;
+  coord_free([x, y]) {
+    return x >= 0 && x < this.width
+        && y >= 0 && y < this.height
+        && this.field[x][y] === 0;
+  }
+
+  coords_free(cs) {
+    return cs.every(p => this.coord_free(p));
+  }
+
+  coord_to_px([x, y]) {
+    return [
+      this.blk_width * x,
+      this.blk_width * (this.height - (y+1)),
+    ];
+  }
+
+  coords_fill(coords, clr) {
     this.ctx.fillStyle = clr;
-    this.ctx.fillRect(x*this.blk_width, y*this.blk_width,
-                      this.blk_width, this.blk_width);
+    for (let [x, y] of coords.map(c => this.coord_to_px(c)))
+      this.ctx.fillRect(
+        x, y,
+        this.blk_width, this.blk_width
+      );
   }
 
-  fill_blk(pos, name) {
-    this._write_blk(pos, name.codePointAt(0), PIECES[name].clr);
+  coords_unfill(cs) { this.coords_fill(cs, this.bg_clr); }
+
+  coords_set(cs, name) {
+    name = name !== 0 && Number.isInteger(name)
+      ? String.fromCharCode(name) : name
+
+    let clr =
+      name === 0 ? this.bg_clr : PIECES[name].clr;
+
+    this.coords_fill(cs, clr);
+
+    for (const [x, y] of cs)
+      this.field[x][y] = name && name.charCodeAt(0);
   }
 
-  clear_blk(pos){
-    this._write_blk(pos, false, this.bg_clr);
+  coords_unset(cs) { this.coords_set(cs, 0); }
+}
+
+class Piece {
+  constructor(blk, [x, y], state = 0) {
+    const coords =
+      blk.map(([dx, dy]) => [x + dx, y + dy]);
+
+    Object.defineProperties(this, {
+      blk:    { writable: false, value: blk    },
+      x:      { writable: false, value: x      },
+      y:      { writable: false, value: y      },
+      st:     { writable: false, value: state  },
+      coords: { writable: false, value: coords },
+    });
+  }
+
+  static rotr(p) {
+    return new Piece(
+      p.blk.map(([x, y]) => [y, -x]),
+      [p.x, p.y],
+      p.st < 3 ? p.st+1 : 0
+    )
+  }
+
+  static rotl(p) {
+    return new Piece(
+      p.blk.map(([x, y]) => [-y, x]),
+      [p.x, p.y],
+      p.st > 0 ? p.st-1 : 3
+    )
+  }
+
+  static trans(p, [dx, dy]) {
+    return new Piece(
+      p.blk,
+      [p.x + dx, p.y + dy],
+      p.st
+    );
   }
 }
 
@@ -58,57 +174,123 @@ function pick_elem(xs) {
   return xs[Math.floor(xs.length * Math.random())];
 }
 
-function rotr(b) {
-  return b.map(([x, y]) => [-y, x]);
+class State {
+  constructor(id, dims, bg) {
+    return {
+      clr:   null, // colour
+      name:  null, // piece name
+      p:     null, // piece
+      loss:  false,
+      field: new Field(id, dims, bg),
+    }
+  }
+
+  static rst(st) {
+    st.field.clear();
+    this.new_pc(st)
+  }
+
+  static tick(st) {
+    const n = Piece.trans(st.p, [0, -1]).coords;
+    if (st.field.coords_free(n)) {
+      this.trans(st, [0, -1]);
+      return;
+    }
+    this.set_pc(st)
+    this.new_pc(st)
+  }
+
+  static new_pc(st) {
+    st.name = pick_elem(PIECE_NAMES);
+    st.clr  = PIECES[st.name].clr;
+    st.p    = new Piece(PIECES[st.name].blk, [4, st.field.height - 2]);
+
+    if (!st.field.coords_free(st.p.coords)) {
+      st.loss = true;
+      return;
+    }
+
+    st.field.coords_fill(st.p.coords, st.clr);
+  }
+
+  static set_pc(st) {
+    st.field.coords_set(st.p.coords, st.name);
+
+    let cleared = st.p.coords
+      .map(([_, y]) => y)
+      .filter(y => {
+        for (let x = 0; x < st.field.width; ++x)
+          if (st.field.coord_free([x, y]))
+            return false;
+        return true;
+      })
+      .sort((a, b) => b - a); // reverse sort
+
+    cleared = new Set(cleared)
+
+    for (const y of cleared)
+      this.clear_row(st, y)
+  }
+
+  static clear_row(st, y) {
+    for (; y < st.field.height - 1; ++y)
+      for (let x = 0; x < st.field.width; ++x)
+        if (st.field.field[x][y] !== st.field.field[x][y+1])
+          st.field.coords_set(
+            [[x, y]],
+            st.field.field[x][y+1]
+          );
+  }
+
+  static _mv(st, f) {
+    st.field.coords_unfill(st.p.coords);
+    st.p = f(st.p);
+    st.field.coords_fill(st.p.coords, st.clr);
+  }
+
+  static trans(st, [dx, dy]) {
+    this._mv(st, (p) => {
+      let np = Piece.trans(p, [dx, dy]);
+
+      if (st.field.coords_free(np.coords))
+        return np;
+      else
+        return p;
+    });
+  }
+
+  static rotr(st) {
+    this._mv(st, (p) => {
+      let np = Piece.rotr(p);
+      let offs =
+        get_offsets(st.name, p.st, np.st)
+        .map(off => Piece.trans(np, off));
+
+      for (const x of offs)
+        if (st.field.coords_free(x.coords))
+          return x;
+
+      return p;
+    });
+  }
 }
 
 async function main(id) {
-  let field = new Field(id, [10, 20], '#eeeeee');
-  field.clear();
-
-  const piece_names = Object.keys(PIECES);
-
-  let piece_name;
-  let piece;
-  let b;    // current piece
-  let x, y; // coordinates
-
-  let fill = (f) => {
-    for (const [w, h] of b) f([x+w, y+h]);
-  };
-
-  let move = (f) => {
-    fill((pos) => field.clear_blk(pos));
-    f(); // apply given transformation
-    fill((pos) => field.fill_blk(pos, piece_name));
-  }
-
-  let translate = (dx, dy) =>
-    move(() => {x += dx, y += dy});
-
-  let rotate = () =>
-    move(() => {b = rotr(b)});
-
+  let st = new State(id, [10, 20], '#eeeeee')
   document.addEventListener('keydown', (e) => {
     switch (e.key) {
-      case 'ArrowLeft'  : translate(-1, +0); break;
-      case 'ArrowRight' : translate(+1, +0); break;
-      case 'ArrowDown'  : translate(+0, +1); break;
-      case 'ArrowUp'    : rotate();          break;
+      case 'ArrowLeft'  : State.trans(st, [-1, +0]); break;
+      case 'ArrowRight' : State.trans(st, [+1, +0]); break;
+      case 'ArrowDown'  : State.trans(st, [+0, -1]); break;
+      case 'ArrowUp'    : State.rotr(st);            break;
     }
   });
 
-  while (true) {
-    piece_name = pick_elem(piece_names);
-    piece      = PIECES[piece_name];
-
-    [x,y] = [4,0];
-    b     = piece.blk;
-    translate(0, 0); // draw piece
-    while (y < field.height) {
-      await sleep(1000);
-      translate(0, 1);
-    }
-    field.clear(0);
+  State.rst(st)
+  while (!st.loss) {
+    await sleep(1000);
+    State.tick(st)
   }
+
+  console.log(":(")
 }
